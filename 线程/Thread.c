65219fe3c7da7e39c55adf6735de43862b0c3cd4
jifukui <805013833@qq.com>
2019-05-18 09:40:53 +0800

    添加线程创建函数

---
+++ 线程/Thread.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int i=0;
+int output();
+int main()
+{
+    int status;
+    pthread_t pthread_id;
+    status=pthread_creat(&pthread_id,NULL,output,NULL);
+    if(status)
+    {
+        printf("创建线程失败\n");
+    }
+    else
+    {
+        printf("创建线程成功\n");
+    }
+    
+    while (i<200)
+    {
+        printf("The father %d\n",i);
+    }
+    return 0;
+}
+int output()
+{
+    while (i<200)
+    {
+        printf("The thread %d\n",i);
+    }
+    return 0;
+}
