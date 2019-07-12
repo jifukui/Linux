444221a15088ba56716cb4ffdc5c0101148a0ab5
jifukui <805013833@qq.com>
2019-07-12 10:31:52 +0800

    编译过程实例

---
+++ 编译过程/main.s
@@ -0,0 +1,37 @@
+	.file	"main.c"
+	.section	.rodata
+.LC0:
+	.string	"The c is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$32, %esp
+	movl	$1, 28(%esp)
+	movl	$2, 24(%esp)
+	movl	24(%esp), %eax
+	movl	28(%esp), %edx
+	addl	%edx, %eax
+	movl	%eax, 20(%esp)
+	movl	20(%esp), %eax
+	movl	%eax, 4(%esp)
+	movl	$.LC0, (%esp)
+	call	printf
+	movl	$0, %eax
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-16)"
+	.section	.note.GNU-stack,"",@progbits
