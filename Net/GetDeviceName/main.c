82b2104f7deb4b56592980e7834589aaa209c926
jifukui <805013833@qq.com>
2019-11-12 13:10:15 +0800

    添加获取设备名得到IP地址的程序

--- Net/GetDeviceName/main.c
+++ Net/GetDeviceName/main.c
@@ -12,12 +12,12 @@ int main(int argc,char**argv)
         fprintf(stderr,"Usage :getip address\n");
         extit(1);
     }
-    if(h=gethostbyname(argc[1])==NULL)
+    if(h=gethostbyname(argv[1])==NULL)
     {
         herror("Gethostbyname\n");
         exit(1);
     }
     printf("Host name :%s\n",h->h_name);
-    printf("IP Address:%s\n",inet_ntoa(*((struct in_addr*)h->h_haddr)));
+    printf("IP Address:%s\n",inet_ntoa(*((struct in_addr*)h->h_addr)));
     return 0;
 }
