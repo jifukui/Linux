de2f473beb36fc3c17525dd276936ff3467b1409
jifukui <805013833@qq.com>
2019-11-14 17:19:25 +0800

    添加部分apue函数

--- apue/error.c
+++ apue/error.c
@@ -1,7 +1,7 @@
 #include "apue.h"
 #include <errno.h>
 #include <stdarg.h>
-static void err_doit(int,int,const char *,va_list)
+static void err_doit(int,int,const char *,va_list);
 void err_ret(const char *fmt,...)
 {
     va_list ap;
