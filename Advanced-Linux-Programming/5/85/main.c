54778cd1db35c960a9d4b5894b7ec06a268e67b2
jifukui <805013833@qq.com>
2019-11-20 14:37:09 +0800

    完成信号量是的进程同步

--- Advanced-Linux-Programming/5/85/main.c
+++ Advanced-Linux-Programming/5/85/main.c
@@ -2,6 +2,8 @@
 #include <sys/sem.h>
 #include <sys/types.h>
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 /**
  * 创建信号量int semget(key_t key, int nsems, int semflg)
  * key为信号量的标识符，nsems为信号量的数量，semflg为权限的标识符
@@ -10,7 +12,7 @@ union semun
 {
     int val;
     struct semid_ds *buf;
-    unsigned short int array;
+    unsigned short int *array;
     struct seminfo *__buf;
 };
 int binary_semaphore_allocation(key_t key,int sem_flags)
@@ -35,8 +37,8 @@ int binary_semaphore_wait(int semid)
     struct sembuf operations[1];
     operations[0].sem_num=0;
     operations[0].sem_op=-1;
-    operations[0].sem_flag=SEM_UNDO;
-    printf("now is %d\n",(int)getpid());
+    operations[0].sem_flg=SEM_UNDO;
+    printf("waitting is %d\n",(int)getpid());
     return semop(semid,operations,1);
 }
 int binary_semaphore_post(int semid)
@@ -44,21 +46,29 @@ int binary_semaphore_post(int semid)
     struct sembuf operations[1];
     operations[0].sem_num=0;
     operations[0].sem_op=1;
-    operations[0].sem_flag=SEM_UNDO;
-    printf("now is %d\n",(int)getpid());
+    operations[0].sem_flg=SEM_UNDO;
+    printf("process is %d\n",(int)getpid());
     return semop(semid,operations,1);
 }
 int main()
 {
-    int err=binary_semaphore_allocation(1,IPC_CREAT | IPC_EXCL|0666);
-    if(err==-1)
+	//extern int errno;
+	key_t semid;
+	errno=0;
+    int err=0; 
+	semid=binary_semaphore_allocation(1,IPC_CREAT |0666);
+	printf("father is %d semid is %d\n",getpid(),(int)semid);
+    if(semid==-1)
     {
-        printf("error of greated\n");
+        printf("error of greated %d %s\n",errno,strerror(errno));
+		exit(1);
     }
-    err=binary_semaphore_initialize(1);
+	errno=0;
+    err=binary_semaphore_initialize(semid);
     if(err==-1)
     {
-        printf("init error\n");
+        printf("init error %d %s\n",errno,strerror(errno));
+		exit(2);
     }
     int i=0;
     for(i;i<5;i++)
@@ -70,10 +80,42 @@ int main()
         }
         else if(err==0)
         {
-            printf("now is %d\n",(int)getpid());
-            binary_semaphore_wait(1);
-            binary_semaphore_post(1);
+            printf("great is %d\n",(int)getpid());
+            binary_semaphore_wait(semid);
+            binary_semaphore_post(semid);
+			printf("end of this %d\n",getpid());
+			exit(0);
         }
         
     }
+	printf("Hello ,this is father\n");
+	int num=0;
+	pid_t childpid;
+	do{
+		childpid=wait(NULL);
+		if(childpid==-1)
+		{
+			if(errno==ECHILD)
+			{
+				printf("all of child have over\n");
+				err=binary_semaphore_deallocate(semid);
+				if(err==-1)
+				{
+					printf("error is %d info is %s\n",errno,strerror(errno));
+				}
+				break;
+			}
+			else
+			{
+				printf("have some error\n");
+			}
+		}
+		else
+		{
+			num++;
+			printf("father have know The child %d have over is the %d\n",(int)childpid,num);
+		}
+	}while(num<6);
+	printf("Hello father have end\n");	
+	return 0;
 }
