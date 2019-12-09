c8f27769014c7ce9ddb6a2fe8b384120554eef01
jifukui <805013833@qq.com>
2019-12-09 17:21:17 +0800

    添加等待线程

---
+++ Advanced-Linux-Programming/4/52/main.c
@@ -0,0 +1,30 @@
+#include <pthread.h>
+#include <stdio.h>
+struct data
+{
+    char value;
+    int count;
+};
+void * char cprint(void *value)
+{
+    struct data *p=(struct data *)value;
+    int i;
+    for(i=0;i<p->count;++i)
+    {
+        fputc(p->value,stderr);
+    }
+    return NULL;
+}
+int main()
+{
+    pthread_t th1;
+    pthread_t th2;
+    struct data thd1;
+    struct data thd2;
+    thd1.value='x';
+    thd1.count=30000;
+    pthread_ctreate(&th1,NULL,&cprint,&thd1);
+    thd2.value='o';
+    thd2.count=20000;
+    pthread_ctreate(&th2,NULL,&cprint,&thd2);
+}
